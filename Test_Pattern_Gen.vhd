e503699ee956ebcdedc1822ff98199942ee5e981
AuricOls <112778782+AuricOls@users.noreply.github.com>
2023-03-16 15:48:17 -0600

    Implement Gumnut

--- Test_Pattern_Gen.vhd
+++ Test_Pattern_Gen.vhd
@@ -26,8 +26,7 @@ entity Test_Pattern_Gen is
     o_Red_Video : out std_logic_vector(g_VIDEO_WIDTH-1 downto 0);
     o_Grn_Video : out std_logic_vector(g_VIDEO_WIDTH-1 downto 0);
     o_Blu_Video : out std_logic_vector(g_VIDEO_WIDTH-1 downto 0);
-	 
-	 output_collision:out std_logic_vector(1 downto 0)
+	  output_collision:out std_logic_vector(1 downto 0)
     );
 end entity Test_Pattern_Gen;
 
@@ -50,61 +49,34 @@ architecture RTL of Test_Pattern_Gen is
       );
   end component Sync_To_Count;
 
-  function draw_Circle_func (signal col, row, posX, posY, r : integer) 
-  return boolean is
-    type array_I is array (natural range <>) of integer;
-    type array_LV is array (0 to 2) of std_logic_vector(9 downto 0);
-    variable sinT, cosT : array_I(15 downto 0);
-    variable v : array_LV;
+  component videoGame_stage2 is 
+    generic (
+      g_ACTIVE_COLS : integer;
+      g_ACTIVE_ROWS : integer
+    );
+    port(
+      i_col : in integer range 0 to g_ACTIVE_COLS;
+      i_row : in integer range 0 to g_ACTIVE_ROWS;
+      i_clk : in std_logic;
+      o_drawShip : out boolean
+    );
+  end component videoGame_stage2;
 
-    variable drawBcircle : boolean := false;
-  begin
-    -- 6, 4, 3
-    -- 7, 6, 5
-    v(0) := "000" & std_logic_vector(to_unsigned(r*3, v(0)'length))(9 downto 3);
-    v(1) := "000" & std_logic_vector(to_unsigned(r*6, v(1)'length))(9 downto 3);
-    v(2) := "000" & std_logic_vector(to_unsigned(r*7, v(2)'length))(9 downto 3);
-  
-    cosT(0) := posX + r; cosT(1) := posX + to_integer(unsigned(v(2))); cosT(2) := posX + to_integer(unsigned(v(1))); cosT(3) := posX + to_integer(unsigned(v(0))); cosT(4) := posX;
-    cosT(5) := posX - to_integer(unsigned(v(0))); cosT(6) := posX - to_integer(unsigned(v(1))); cosT(7) := posX - to_integer(unsigned(v(2))); cosT(8) := posX - r; cosT(9) := posX - to_integer(unsigned(v(2)));
-    cosT(10) := posX - to_integer(unsigned(v(1))); cosT(11) := posX - to_integer(unsigned(v(0))); cosT(12) := posX; cosT(13) := posX + to_integer(unsigned(v(0))); cosT(14) := posX + to_integer(unsigned(v(1)));
-    cosT(15) := posX + to_integer(unsigned(v(2)));
- 
-    sinT(4) := posY; sinT(3) := posY + to_integer(unsigned(v(0))); sinT(2) := posY + to_integer(unsigned(v(1))); sinT(1) := posY + to_integer(unsigned(v(2))); sinT(0) := posY + r;
-    sinT(15) := posY + to_integer(unsigned(v(2))); sinT(14) := posY + to_integer(unsigned(v(1))); sinT(13) := posY + to_integer(unsigned(v(0))); sinT(12) := posY; sinT(11) := posY - to_integer(unsigned(v(0)));
-    sinT(10) := posY - to_integer(unsigned(v(1))); sinT(9) := posY - to_integer(unsigned(v(2))); sinT(8) := posY - r; sinT(7) := posY - to_integer(unsigned(v(2))); sinT(6) := posY - to_integer(unsigned(v(1)));
-    sinT(5) := posY - to_integer(unsigned(v(0)));
-
-    if (col>cosT(8) and col<cosT(7) and (row>sinT(5) and row<sinT(3))) then
-      drawBcircle := true; 
-    -- if (row>sinT(5) and row<sinT(3)) then
-    --   drawBcircle := true;
-    elsif ((col>cosT(7) and col<cosT(6) and row>sinT(6) and row<sinT(5)) = true) then
-      drawBcircle := true;
-    elsif ((col>cosT(6) and col<cosT(5) and row>sinT(7) and row<sinT(6)) = true) then
-      drawBcircle := true;                    
-    elsif ((col>cosT(5) and col<cosT(3) and row>sinT(8) and row<sinT(7)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(3) and col<cosT(2) and row>sinT(7) and row<sinT(6)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(2) and col<cosT(1) and row>sinT(6) and row<sinT(5)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(1) and col<cosT(0) and row>sinT(5) and row<sinT(3)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(2) and col<cosT(1) and row>sinT(3) and row<sinT(2)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(3) and col<cosT(2) and row>sinT(2) and row<sinT(1)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(5) and col<cosT(3) and row>sinT(1) and row<sinT(0)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(6) and col<cosT(5) and row>sinT(2) and row<sinT(1)) = true) then
-      drawBcircle := true; 
-    elsif ((col>cosT(7) and col<cosT(6) and row>sinT(3) and row<sinT(2)) = true) then
-      drawBcircle := true; 
-    end if;
-
-    return drawBcircle;
-  end function;
+  component draw_circle is
+    generic (
+     g_ACTIVE_COLS : integer := 640;
+     g_ACTIVE_ROWS : integer := 480
+    );
+    port(
+        i_col : in integer range 0 to g_ACTIVE_COLS;
+        i_row : in integer range 0 to g_ACTIVE_ROWS;
+        i_posX : in integer range 0 to g_ACTIVE_COLS;
+        i_posY : in integer range 0 to g_ACTIVE_COLS;
+        i_r : integer range 0 to g_ACTIVE_COLS;
+        i_clk : in std_logic;
+        o_drawCircle : out boolean
+    );
+  end component draw_circle;
 
   -- VGA signal
   signal w_VSync : std_logic;
@@ -125,13 +97,14 @@ architecture RTL of Test_Pattern_Gen is
   signal row : integer := to_integer(unsigned(w_Row_Count));
   
   -- For drawing frame
-  signal drawBstar, drawCirclesig : boolean;
+  signal drawBstar, drawBstar_2, drawBcircle: boolean;
 
   -- For video game
-  signal r : integer range 0 to 100 := 50;
+  type stage_type is (stage0, stage1);
+  signal stage : stage_type := stage0;
 
-  signal wStar : integer range 0 to 100 := 80;
-  signal hStar : integer range 0 to 100 := 55;
+  signal wStar : integer range 0 to 100 := 60;
+  signal hStar : integer range 0 to 100 := 45;
   signal posXStar : integer range 0 to g_ACTIVE_COLS - wStar := 380;
   signal posYStar : integer range 0 to g_ACTIVE_ROWS - hStar := 400;
 
@@ -148,9 +121,9 @@ architecture RTL of Test_Pattern_Gen is
   signal ob_H : obstacleH := (others => 75);
   signal ob_step : obstacleStep := (0 => 10, 1 => 5, 2 => 3, 3 => 15 , 4 => 10);
   signal ob_draw : drawObstacle;
-  
-  signal counter_collision:integer range 0 to 50000:=0;
 
+  -- For circle
+  signal r : integer range 0 to 100 := 10;
 
 begin
 
@@ -169,6 +142,33 @@ begin
       o_Row_Count => w_Row_Count
       );
 
+    stage2_game_inst : videoGame_stage2
+      generic map(
+        g_ACTIVE_COLS => g_ACTIVE_COLS,
+        g_ACTIVE_ROWS => g_ACTIVE_ROWS
+      )
+     port map(
+         i_col => col,
+         i_row => row,
+         i_clk => i_clk,
+         o_drawShip => drawBstar_2
+     );
+
+     circle : draw_circle
+     generic map(
+       g_ACTIVE_COLS => g_ACTIVE_COLS,
+       g_ACTIVE_ROWS => g_ACTIVE_ROWS
+     )
+    port map(
+        i_col => col,
+        i_row => row,
+        i_clk => i_clk,
+        i_posX => posXStar,
+        i_posY => posYStar,
+        i_r => r,
+        o_drawCircle => drawBcircle
+    );
+
 
   -- Register syncs to align with output data.
   p_Reg_Syncs : process (i_Clk) is
@@ -176,17 +176,31 @@ begin
     if rising_edge(i_Clk) then
       o_VSync <= w_VSync;
       o_HSync <= w_HSync;
-		---------------------
-		counter_collision<=counter_collision+1;
-		--------------
     end if;
   end process p_Reg_Syncs; 
+  
+  --Sends collision
+--  output_collision <= "00" when ((drawBstar and ob_draw(0)) or (drawBstar and ob_draw(1))
+--	or (drawBstar and ob_draw(2)) or (drawBstar and ob_draw(3))) else "11";
 
-	-- sending collision
-	
-	output_collision <= "11" when ((drawBstar and ob_draw(0)) or (drawBstar and ob_draw(1))
-	or (drawBstar and ob_draw(2)) or (drawBstar and ob_draw(3))) else "00";
-	
+    -- For choosing stage in game (Pattern 1,2)
+  stage_counter : process (i_clk) is
+    variable c_bn : integer range 0 to 5;
+  begin
+    if(i_key(0) = '1') then
+      c_bn:=0;
+    end if;
+    if rising_edge(i_clk) then
+      if (posYStar <= 10 and stage = stage0) then
+        stage <= stage1;
+      end if;
+      if (i_key(0) = '0') then
+        stage <= stage0;
+        c_bn := c_bn +1;
+      end if;
+    end if;
+  end process;
+
   -----------------------------------------------------------------------------
   -- Pattern 0: Disables the Test Pattern Generator
   -----------------------------------------------------------------------------
@@ -197,8 +211,9 @@ begin
   Pattern_Blu(0) <= Pattern_Red(0);
   
   -----------------------------------------------------------------------------
-  -- Pattern 1: Video Game
+  -- Pattern 1: Video Game stage 0
   -----------------------------------------------------------------------------
+
 
   drawBstar <= col > posXStar and 
   col < posXStar + wStar and
@@ -233,21 +248,30 @@ begin
   Pattern_Grn(1) <= (others => '0');
   Pattern_Blu(1) <= (others => '1') when (drawBstar = True) else
                     (others => '0');
+
   -----------------------------------------------------------------------------
-  -- Pattern 2: Circle
+  -- Pattern 2: Video Game Stage 2
   -----------------------------------------------------------------------------
-  Pattern_Red(2) <= (others => '1') when ( draw_Circle_func(col, row, posXStar, posYStar, r) = True) else
+  Pattern_Red(2) <= (others => '1') when (drawBstar_2 = true) else
                     (others => '0');
-  Pattern_Grn(2) <= (others => '0');
-    Pattern_Blu(2) <= Pattern_Red(2);
+  Pattern_Grn(2) <= (others => '1')  when (drawBstar_2 = true) else
+                    (others => '0');
+  Pattern_Blu(2) <= (others => '1') when (drawBstar_2 = True) else
+                    (others => '0');
+      
+  -----------------------------------------------------------------------------
+  -- Pattern 3: Circle
+  -----------------------------------------------------------------------------
+  Pattern_Red(3) <= (others => '1') when ( drawBcircle = True) else
+                    (others => '0');
+  Pattern_Grn(3) <= (others => '0');
+    Pattern_Blu(3) <= Pattern_Red(3);
 
 
 
  -----------------------------------------------------------------------------
-  -- Process to control FRAME
+  -- Process to control FRAME Video Game Stage 1
   -----------------------------------------------------------------------------
-
-
   -- updates starship position
   starship_actions : process(i_Clk)
       constant step : integer := 20;
@@ -258,6 +282,16 @@ begin
 
   begin
     if rising_edge(i_clk) then
+      if(drawBstar and ob_draw(0)) or (drawBstar and ob_draw(1)) or
+        (drawBstar and ob_draw(2)) or (drawBstar and ob_draw(3)) 
+        then
+          posXStar <= 380;
+          posYStar <= 400;
+			 output_collision <= "00";
+		else 
+			output_collision <= "11";
+      end if;
+
       if i_key(1) = '1' then
         c_bn := 0;
       end if;
@@ -341,13 +375,20 @@ begin
           o_Grn_Video <= Pattern_Grn(0);
           o_Blu_Video <= Pattern_Blu(0);
         when "0001" =>
-          o_Red_Video <= Pattern_Red(1);
-          o_Grn_Video <= Pattern_Grn(1);
-          o_Blu_Video <= Pattern_Blu(1);
-        when "0010" =>
-          o_Red_Video <= Pattern_Red(2);
-          o_Grn_Video <= Pattern_Grn(2);
-          o_Blu_Video <= Pattern_Blu(2);
+          case stage is 
+            when stage0 =>
+              o_Red_Video <= Pattern_Red(1);
+              o_Grn_Video <= Pattern_Grn(1);
+              o_Blu_Video <= Pattern_Blu(1);
+            when stage1 =>
+              o_Red_Video <= Pattern_Red(2);
+              o_Grn_Video <= Pattern_Grn(2);
+              o_Blu_Video <= Pattern_Blu(2);
+            end case;
+        when "0101" =>
+          o_Red_Video <= Pattern_Red(3);
+          o_Grn_Video <= Pattern_Grn(3);
+          o_Blu_Video <= Pattern_Blu(3);
         when others =>
           o_Red_Video <= Pattern_Red(0);
           o_Grn_Video <= Pattern_Grn(0);
